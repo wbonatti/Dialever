10cdc5ea64efaf4cd77d67859b763ee7fd6366c7
wbonatti <w.bonatti@hotmail.com>
2017-08-05 16:59:14 -0300

    Html check.
    Fix html tag

--- index.php
+++ index.php
@@ -1,24 +1,25 @@
+<html>
 
-<body>
-<a href="/Dialever/public/index.php/payments">Metodos de pagamento</a>
-<br>
-<a href="/Dialever/public/index.php/customers">Clientes</a>
-<br>
-<a href="/Dialever/public/index.php/notifications">Notificacoes</a>
-<br>
-<a href="/Dialever/public/index.php/products">Produtos</a>
-<br>
-<a href="/Dialever/public/index.php/proposals">Propostas</a>
-<br>
-<a href="/Dialever/public/index.php/users">Usuarios</a>
-<br>
-<a href="/Dialever/public/index.php/companies">Empresas</a>
-<br>
-<a href="/Dialever/public/index.php/mailConfigs">Configuracao</a>
+	<body>
+		<a href="/Dialever/public/index.php/payments">Metodos de pagamento</a>
+		<br>
+		<a href="/Dialever/public/index.php/customers">Clientes</a>
+		<br>
+		<a href="/Dialever/public/index.php/notifications">Notificacoes</a>
+		<br>
+		<a href="/Dialever/public/index.php/products">Produtos</a>
+		<br>
+		<a href="/Dialever/public/index.php/proposals">Propostas</a>
+		<br>
+		<a href="/Dialever/public/index.php/users">Usuarios</a>
+		<br>
+		<a href="/Dialever/public/index.php/companies">Empresas</a>
+		<br>
+		<a href="/Dialever/public/index.php/mailConfigs">Configuracao</a>
 
 
 
-</body>
+	</body>
 
 </html>
     
